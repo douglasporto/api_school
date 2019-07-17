0fc1fd15c2110445c8ff7b97dfe2755b88269d1c
Douglas Porto <douglasalexandre7@gmail.com>
2019-07-17 13:05:29 -0300

    Take off schedule

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,6 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
-import ScheduleController from './app/controllers/ScheduleController';
 import NotificationsController from './app/controllers/NotificationsController';
 
 const routes = new Router();
@@ -18,8 +17,6 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
-
-routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationsController.index);
 routes.put('/notifications/:id', NotificationsController.update);
